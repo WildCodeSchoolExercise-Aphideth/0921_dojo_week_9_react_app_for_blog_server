6fc1d53b24d4ff3dd47bede2416f9a3a71615a7f
JennyViannay <jenny.viannay75@gmail.com>
2021-11-09 08:01:21 +0100

    start here

--- src/components/articles/Articles.jsx
+++ src/components/articles/Articles.jsx
@@ -1,33 +1,8 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-
-import ArticleCard from "./ArticleCard";
-
 const Articles = () => {
-    const [articles, setArticles] = useState([]);
-
-    useEffect(() => {
-        getAllArticles()
-    }, []);
-    
-    const getAllArticles = () => {
-        axios.get("http://localhost:5000/articles")
-            .then(response => response.data)
-            .then(data => setArticles(data));
-    }
-    const deleteArticle = (id) => {
-        axios.delete(`http://localhost:5000/articles/${id}`)
-            .then(response => response.data)
-        getAllArticles()
-    }
-
     return (
         <div className="flex flex-wrap">
-            {articles ?
-                articles.map(article => (
-                    <ArticleCard key={article.id} article={article} deleteAction={deleteArticle} />
-                ))
-                : "No data"}
+            {/* TODO:: display all articles - 
+            Tips: you will have to use the child component ArticleCard.jsx with props */}
         </div>
     )
 }
