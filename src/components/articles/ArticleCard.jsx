a8425e9c8dd71d7b3b9a720b6d2b23f0cdad20c9
JennyViannay <jenny.viannay75@gmail.com>
2021-11-10 16:17:42 +0100

    add read all & read one article more delete one article

--- src/components/articles/ArticleCard.jsx
+++ src/components/articles/ArticleCard.jsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
-//import { FaTrash } from 'react-icons/fa';
+import { FaTrash } from 'react-icons/fa';
 
-
-const ArticleCard = () => {
+// component enfant recevant les props : les infos d'un article et la fonction deleteArticle
+const ArticleCard = ({ article, deleteArticle }) => {
     return (
         <div className="p-10">
             <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                <Link to={"/article/:id"}>
-                    <img className="w-full" src="::TODO article image src" alt="Mountain" />
+                <Link to={`/article/${article.id}`}>
+                    <img className="w-full" src={article.image} alt="Mountain" />
                     <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-2">TODO:: article title</div>
-                        <p className="text-gray-700 text-base">TODO:: article content</p>
+                        <div className="font-bold text-xl mb-2">{article.title}</div>
+                        <p className="text-gray-700 text-base">{article.content}</p>
                     </div>
                 </Link>
                 <div className="px-6 pt-4 pb-2">
@@ -19,12 +19,13 @@ const ArticleCard = () => {
                     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
                 </div>
                 <div className="px-6 pt-4 pb-2">
-                    {/* 
-                    TODO:: Add Action Delete to ArticleCard and make it available to the parent component
-                    Decomment Line 2 to allaow React to use FaTrash component
-                    <button class="bg-green-900 hover:bg-green-800 text-white font-bold py-2 px-4 rounded-full" onClick={}>
+                    <button
+                        className="bg-green-900 hover:bg-green-800 text-white font-bold py-2 px-4 rounded-full"
+                        // quand j'appelle une methode directement dans le onClick je dois la déclencher dans une fonction anonyme : () => {maMethode()}
+                        // car sinon la methode deleteArticle provoquera des effets de bord inatendus 
+                        onClick={() => deleteArticle(article.id)}>
                         <FaTrash />
-                    </button> */}
+                    </button>
                 </div>
             </div>
         </div>
--- src/components/articles/Articles.jsx
+++ src/components/articles/Articles.jsx
@@ -1,8 +1,45 @@
+import { useState, useEffect } from 'react';
+import axios from "axios";
+import ArticleCard from './ArticleCard';
+// Component Parent :
+// Contient les datas et les methodes pour mes enfants
 const Articles = () => {
+    // init d'un state pour recevoir tous les articles
+    const [articles, setArticles] = useState([]);
+
+    // lorsque le compononent sera monté, useEffect appelera la methode getAllArticles
+    useEffect(() => {
+        getAllArticles()
+    }, [])
+
+    // getAllArticles permet de requêter le serveur sur l'URi permettant d'obtenir tous les articles depuis la bdd
+    const getAllArticles = () => {
+        axios.get('http://localhost:5000/articles')
+            .then(res => res.data)
+            .then(data => setArticles(data))
+    }
+
+    // deleteArticle permet de requêter le serveur sur l'URi permettant de supprimer un article en bdd
+    // la methode prend en parametre id qui prendra pour valeur l'id de l'article à supprimer
+    const deleteArticle = (id) => {
+        axios.delete(`http://localhost:5000/articles/${id}`)
+            .then(res => res.data)
+        // une fois que j'ai supprimé un article en bdd, pour que mon state 'articles' se mette,
+        // je refais appel à la methode getAllArticles qui renvoie une requête vers le serveur et récupère tous les articles en bdd  
+        getAllArticles()
+    }
+
     return (
         <div className="flex flex-wrap">
-            {/* TODO:: display all articles - 
-            Tips: you will have to use the child component ArticleCard.jsx with props */}
+            {articles ?
+                articles.map(article => (
+                    // compononent enfant me permettant d'afficher un article et de lui passer les methodes dont il a besoin
+                    // ici article (toutes les infos d'un article) et deleteArticle (methode pour supprimer un article)
+                    <ArticleCard key={article.id} article={article} deleteArticle={deleteArticle} />
+                ))
+                :
+                <p>Nothing to show</p>
+            }
         </div>
     )
 }
